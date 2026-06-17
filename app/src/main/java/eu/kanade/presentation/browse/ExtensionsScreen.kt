ac513ca489421d80ea9eb94479775e8a1111c4ce
krozz <krozzgaming9@gmail.com>
2026-06-17 15:00:41 +0200

    Forgotten imports

--- app/src/main/java/eu/kanade/presentation/browse/ExtensionsScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/ExtensionsScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import dev.icerock.moko.resources.StringResource
